e9d2d2be50a56d7870430af70327179ab33404df
Soudeepta146 <soudeepta2004bhattacharjee@gmail.com>
2026-05-06 13:33:18 +0530

    updated frontend

--- Solara/frontend/src/services/api.js
+++ Solara/frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:10000";
+const BASE_URL = "https://solara-project.onrender.com";
+// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:10000";
 // const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 export const getSolarPrediction = async (lat, lon, device = "solar_car") => {
